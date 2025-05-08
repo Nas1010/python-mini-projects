5fd51a362003809fa0f3e0365cd41110a0223f27
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-05-08 18:46:01 +0100

    Score adding and deducting to hangman.py

---
+++ hangman.py
@@ -0,0 +1,23 @@
+import random
+
+words = ['sheila', 'eric', 'eva', 'daisy', 'inspector']
+
+#randomly choose a word from the list
+
+chosen_word = random.choice(words)
+word_display = ['_' for _ in chosen_word]
+print(word_display)
+attempts = 8
+
+print('Welcome to Hangman - Inspector Calls Edition!')
+
+while attempts > 0 and '_' in word_display:
+    print('\n' + ' '.join(word_display))
+    guess = input('Guess the letter: ').lower()
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word): 
+            if letter == guess: 
+             word_display[index] = guess
+    else:
+       print('That letter does not appear in the word.')
+       attempts -= 1
