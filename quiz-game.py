ae33da4db52e773b4d1504cd62eea300aea4b5a1
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-05-08 17:10:20 +0100

    Create list for the quiz-game

---
+++ quiz-game.py
@@ -0,0 +1,27 @@
+questions = [
+    {
+        'prompt': 'Who is Eva Smith most closely associated with in the play’s timeline before her death?',
+        'options': ['A) Edna', 'B) Sheila', 'C) All of the Birlings and Gerald', 'D) Inspector Goole'],
+        'answer': 'C) All of the Birlings and Gerald',
+    },
+    {
+        'prompt': 'What theme does the Inspector’s final speech most strongly convey?',
+        'options': ['A) Capitalism', 'B) Love and family', 'C) Social responsibility and collective guilt', 'D) Gender roles'],
+        'answer': 'C) Social responsibility and collective guilt',
+    },
+    {
+        'prompt': 'What does the Inspector’s name suggest about his character?',
+        'options': ['A) He is a ghost', 'B) He is a policeman', 'C) He is a priest', 'D) He is a teacher'],
+        'answer': 'A) He is a ghost',
+    },
+    {
+        'prompt': 'What does the Inspector’s arrival signify in the play?',
+        'options': ['A) A change in the weather', 'B) The end of the Birlings’ happiness', 'C) The beginning of a new era', 'D) The arrival of a new character'],
+        'answer': 'B) The end of the Birlings’ happiness',
+    },
+    {
+        'prompt': 'What does the Inspector’s final speech suggest about the future?',
+        'options': ['A) It will be better', 'B) It will be worse', 'C) It will be the same', 'D) It is uncertain'],
+        'answer': 'D) It is uncertain',
+    }
+]
