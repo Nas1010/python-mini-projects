7a35c172828406d7953db331293d91c63d40a770
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2025-05-08 20:31:16 +0100

    feat: add budget tracker with expense logging, total spend and remaining balance calculation

--- budget-tracker.py
+++ budget-tracker.py
@@ -1,7 +1,23 @@
-def add_expenses(expenses, description, amount):
+def add_expense(expenses, description, amount):
     expenses.append({'expenses': expenses, 'amount': amount})
     print(f'Added expense: {description}, Amount: {amount}')
 
+def get_total_expenses(expenses):
+    sum = 0
+    for expense in expenses:
+        sum += expense['amount']
+    return sum
+
+def get_balance(budget, expenses):
+    return budget -  get_total_expenses(expenses)
+
+def show_budget_details(budget, expenses):
+    print(f'Total budget: {budget}')
+    print('Expenses: ')
+    for expense in expenses:
+        print(f'- {expense['description']}: {expense['amount']}')
+        print(f'Total spend: {get_total_expenses(expenses)}')
+        print(f'Remaining budget: {get_balance(budget, expenses)}')
 
 def main():
     print("Welcome to the budget tracker app")
@@ -15,18 +31,18 @@ def main():
         print('2. Show budget details')
         print('3. Exit')
         choice = input('Enter your choice (1/2/3): ')
-
-    if choice == '1':
-        description = input('Enter expense description: ')
-        amount = float(input('Enter expense amount: '))
-        add_expense(expense, description, amount)
-    elif choice == '2':
-        show_budget_details(budget, expenses)
-    elif choice == '3':
-        print('Exiting budget tracker app. Goodbye!')
-        break
-    else:
-        print('Invalid choice, please choose again!')
+    
+        if choice == '1':
+          description = input('Enter expense description: ')
+          amount = float(input('Enter expense amount: '))
+          add_expense(expenses, description, amount)
+        elif choice == '2':
+          show_budget_details(budget, expenses)
+        elif choice == '3':
+          print('Exiting budget tracker app. Goodbye!')
+          break
+        else:
+          print('Invalid choice, please choose again!')
 
 if __name__ == '__main__':
-main()
+ main()
